6db7b9f4bb7a32dcd20dc777e437f358045d9178
dhiraj <dhiraj@dhiraj-Latitude-E6400>
2017-11-07 23:42:08 +0530

    fs

---
+++ learninggit/src/main/java/git/MainRepo/DevelopBranch.java
@@ -0,0 +1,10 @@
+package git.MainRepo;
+
+public class DevelopBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ learninggit/src/main/java/git/MainRepo/MasterBranchChanges.java
@@ -0,0 +1,12 @@
+package git.MainRepo;
+
+public class MasterBranchChanges {
+
+	public static void main(String[] args) {
+	
+		System.out.println(" Master Branch ");
+		
+
+	}
+
+}
